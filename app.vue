0e377897029dae1d4e5c9d660bd562c8e8eb8171
Savva Siry <savvasiry@gmail.com>
2024-01-23 13:49:59 +1100

    init(app): add base folders for work

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+
+<!--  <Profile />-->
+
+<!--  <Alert/>-->
+
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+
+<!--  <NuxtLayout>-->
+<!--    <ul>-->
+<!--      <li>-->
+<!--        <NuxtLink to="/">-->
+<!--          Home-->
+<!--        </NuxtLink>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <NuxtLink to="events">-->
+<!--        Events-->
+<!--      </NuxtLink>-->
+<!--      </li>-->
+<!--    </ul>-->
+<!--  </NuxtLayout>-->
+
+</template>
+<script>
+import Alert from "./components/Alert.vue";
+export default {
+  components: {Alert}
+}
+</script>
