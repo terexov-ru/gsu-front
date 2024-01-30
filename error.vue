9baec389c6dc5e7a71d23967a8cad55d93d3ff1b
Savva Siry <savvasiry@gmail.com>
2024-01-30 16:28:47 +1100

    feature(error): add error page

--- error.vue
+++ error.vue
@@ -5,33 +5,41 @@ const props = defineProps({
 </script>
 
 <template>
+  <AppBar/>
   <div class="wrapper wrapper_paddings">
+    <div class="column error-container">
 
-    <div class="column error-container column_gap20">
-      <div class="text error">{{ error.statusCode }}</div>
+      <img src="/assets/svg/sad_smile.svg" alt="error">
+
+      <div class="text text_semi-bold">Ошибка {{ error.statusCode }}</div>
 
       <div class="text_center">
-        <div class="text text_h4 text_light">Вот ты и попал,</div>
-        <div class="text text_h4 text_light">но не туда...</div>
+        <div class="text text_normal">
+          <span class="text_light">Воспользуйтесь поиском по сайту или вернитесь на</span>
+          <span class="text_accent">
+            <NuxtLink to="/">
+              Главную страницу
+            </NuxtLink>
+          </span>
+        </div>
       </div>
 
-      <NuxtLink
-          class="button button_paddings button_gradient"
-          to="/"
-      >
-        Вернуться на главную
-      </NuxtLink>
     </div>
-
   </div>
+  <AppFooter/>
 </template>
 
 <style scoped>
 @import "~/assets/core.less";
 
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
 .error-container {
   width: 100%;
-  height: 100vh;
+  height: 80vh;
 
   display: flex;
   flex-direction: column;
