61f3bb04d2c44fe07fe8720077bde30cb8be7b70
Savva Siry <savvasiry@gmail.com>
2024-01-31 21:36:34 +1100

    feature(AdvSearch): add drop down search menu

--- components/ProgramSearchBar.vue
+++ components/ProgramSearchBar.vue
@@ -2,12 +2,46 @@
   <!--  TODO вынести input как отедльный компонент и добваить slot под icon-->
   <div class="search-container">
     <div class="input-container">
-      <input class="input" :placeholder="!$viewport.isLessThan('desktop') ? 'Поиск по ключевым словам...' : 'Поиск'" type="text">
+      <input
+          @focus="active = true"
+          @focusout="active = false"
+          class="input" :placeholder="!$viewport.isLessThan('desktop') ? 'Поиск по ключевым словам...' : 'Поиск'"
+             type="text">
       <svg class="input-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M21 21L16.6569 16.6569M16.6569 16.6569C18.1046 15.2091 19 13.2091 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19C13.2091 19 15.2091 18.1046 16.6569 16.6569Z"
             stroke="#B9BFC6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
+
+      <div class="search-drop"
+           v-if="active"
+      >
+
+        <div class="search-drop__block">
+          <div class="text search-drop__title text_caption text_accent">лабораторная диагностика</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+        </div>
+
+        <div class="search-drop__block">
+          <div class="text search-drop__title text_caption text_accent">лабораторная диагностика</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+        </div>
+
+        <div class="search-drop__block">
+          <div class="text search-drop__title text_caption text_accent">лабораторная диагностика</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+        </div>
+
+        <div class="search-drop__block">
+          <div class="text search-drop__title text_caption text_accent">лабораторная диагностика</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+          <div class="text search-drop__item text_normal">Медицинская реабилитация как вид помощи при коронавирусной инфекции Covid-19</div>
+        </div>
+
+      </div>
     </div>
     <button class="button button_size button_gradient">Найти</button>
   </div>
@@ -28,7 +62,8 @@ export default {
         {id: 1, text: "Повышение квалификации"},
         {id: 2, text: "Профессиональная переподготовка"},
         {id: 3, text: "Профессиональная переобучение"},
-      ]
+      ],
+      active: false,
     }
   },
   methods: {
@@ -117,5 +152,42 @@ export default {
   position: relative;
   width: 100%;
   display: flex;
+}
+
+.search-drop {
+  position: absolute;
+  left: 0;
+  top: calc(12px + 52px);
+  z-index: 100;
+
+  width: 100%;
+  height: auto;
+  max-height: 300px;
+
+  box-sizing: border-box;
+  padding: 12px 34px 0 16px;
+
+  overflow-y: auto;
+
+  background: @WhiteColor;
+  border-radius: 4px;
+  box-shadow: @BoxShadowEffect;
+}
+
+.search-drop__title {
+  margin-bottom: 12px;
+}
+
+.search-drop__item {
+  margin-bottom: 8px;
+  cursor: pointer;
+
+  &:hover {
+    color: @BlueNewColor;
+  }
+}
+
+.search-drop__block {
+  margin-bottom: 16px;
 }
 </style>
