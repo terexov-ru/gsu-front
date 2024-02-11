8af237d8b06e826a15d34b2e705220f1fad2a4ab
Savva Siry <savvasiry@gmail.com>
2024-02-11 15:37:54 +1100

    feature(ConsultationForm): add consultation form logic

--- components/overflow/consultation.vue
+++ components/overflow/consultation.vue
@@ -1,50 +1,96 @@
 <template>
-  <Overflow>
+  <Overflow
+    v-if="!success"
+  >
     <img class="close"
          @click="this.$emit('close')"
          src="~/assets/svg/close.svg"
          alt="close"
     >
 
-    <div class="form">
+    <Form class="form" @submit="onSubmit">
+
       <InputBlock
-          :name="'ФИО'"
+          :name="'name'"
+          :title="'ФИО'"
           :type="'text'"
-          :placeholder="'Ваше имя'"
+          v-model:value="nameValue"
+          :placeholder="'Иванов Иван Иванович'"
+          :rule="validateName"
       />
 
       <InputBlock
-          :name="'Номер телефона'"
+          :name="'phone'"
+          :title="'Телефон'"
           :type="'text'"
+          v-model:value="phoneValue"
           :placeholder="'Номер телефона'"
+          :mask="phoneMask"
+          :rule="validatePhone"
       />
 
       <InputBlock
-          :name="'E-mail'"
+          :name="'email'"
+          :title="'E-mail'"
           :type="'text'"
-          :placeholder="'mail@mail.ru'"
+          v-model:value="mailValue"
+          :placeholder="'Почта'"
+          :rule="validateEmail"
       />
-    </div>
-    <div class="column column_gap8">
 
-      <div class="text text_normal text_center">
-        <p>Оставляя данные в этой форме, Вы даете</p>
-        <p class="text_accent">Согласие на обработку персональных данных</p>
+      <div class="column column_gap8">
+
+        <div class="text text_normal text_center">
+          <p>Оставляя данные в этой форме, Вы даете</p>
+          <p class="text_accent">Согласие на обработку персональных данных</p>
+        </div>
+
+        <button class="button overflow-card__button button_gradient">Записаться</button>
+        <button
+            :disabled="disabled"
+            class="button overflow-card__button button_black-bordered"
+        >
+          Получить консультацию
+        </button>
       </div>
+    </Form>
 
-      <button class="button overflow-card__button button_gradient">Записаться</button>
-      <button class="button overflow-card__button button_black-bordered">Получить консультацию</button>
-    </div>
   </Overflow>
+
+  <OverflowSuccess
+      v-else
+  />
 </template>
+
+<script setup>
+import {ref} from "vue";
+
+const {validateEmail, validateName, validatePhone, phoneMask} = useValidate();
+const {sendForm} = useApi();
+
+const nameValue = ref('');
+const phoneValue = ref('');
+const mailValue = ref('');
+
+const success = ref(false);
+const disabled = ref(false);
+
+async function onSubmit(values) {
+  disabled.value = true;
+  const {data, status} = await sendForm(values.name, values.email, values.phone, 'Получить консультацию', undefined);
+  if (status.value === 'success' && data.value.status === 'ok') {
+    success.value = true;
+  }
+
+  disabled.value = false;
+}
+</script>
 
 <style scoped lang="less">
 @import "assets/core.less";
 
 
 .form {
-  margin-bottom: 40px;
-
   display: flex;
   flex-direction: column;
   gap: 20px;
