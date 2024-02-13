478d228da8907d6f98f068d52e48de21b1bed334
Savva Siry <savvasiry@gmail.com>
2024-02-13 18:43:53 +1100

    feature(MoreInfo): add form validation for more info

--- components/overflow/moreInfo.vue
+++ components/overflow/moreInfo.vue
@@ -1,49 +1,78 @@
 <template>
-  <Overflow>
+  <OverflowSuccess v-if="success"/>
+  <Overflow v-else>
     <img class="close"
          @click="this.$emit('close')"
          src="~/assets/svg/close.svg"
          alt="close"
     >
 
-    <div class="form">
+    <Form class="form" @submit="onSubmit">
       <InputBlock
-          :name="'ФИО'"
+          :name="'name'"
+          :title="'ФИО'"
           :type="'text'"
-          :placeholder="'Ваше имя'"
+          v-model:value="nameValue"
+          :placeholder="'Иванов Иван Иванович'"
+          :rule="validateName"
       />
 
       <InputBlock
-          :name="'Номер телефона'"
+          :name="'phone'"
+          :title="'Телефон'"
           :type="'text'"
+          v-model:value="phoneValue"
           :placeholder="'Номер телефона'"
+          :mask="phoneMask"
+          :rule="validatePhone"
       />
-    </div>
 
-    <div class="column column_gap8">
+      <div class="column column_gap8">
 
-      <div class="text text_normal text_center">
-        <p>Оставляя данные в этой форме, Вы даете</p>
-        <p class="text_accent">Согласие на обработку персональных данных</p>
+        <div class="text text_normal text_center">
+          <p>Оставляя данные в этой форме, Вы даете</p>
+          <p class="text_accent">Согласие на обработку персональных данных</p>
+        </div>
+
+        <button
+            :disabled="disabled"
+            class="button overflow-card__button button_dark"
+        >
+          Узнать больше
+        </button>
       </div>
+    </Form>
 
-      <button class="button overflow-card__button button_dark">Узнать больше</button>
-    </div>
   </Overflow>
 </template>
 
-<script>
-export default {
-  name: "moreInfo"
+<script setup>
+import {ref} from "vue";
+
+const {validateName, validatePhone, phoneMask} = useValidate();
+const {sendForm} = useApi();
+
+const nameValue = ref('');
+const phoneValue = ref('');
+const success = ref(false);
+const disabled = ref(false);
+
+async function onSubmit(values) {
+  disabled.value = true;
+  const {data, status} = await sendForm(values.name, '', values.phone, 'Узнать больше', '');
+  if (status.value === 'success' && data.value.status === 'ok') {
+    success.value = true;
+  }
+
+  disabled.value = false;
 }
+
 </script>
 
 <style scoped lang="less">
 @import "assets/core.less";
 
 .form {
-  margin-bottom: 40px;
-
   display: flex;
   flex-direction: column;
   gap: 20px;
