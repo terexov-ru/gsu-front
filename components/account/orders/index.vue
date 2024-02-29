2c54eec05020aba0df8a84ec970e65667ad2ccf4
Savva Siry <savvasiry@gmail.com>
2024-02-29 17:33:52 +1100

    feature(Order): add order description

--- components/account/orders/index.vue
+++ components/account/orders/index.vue
@@ -1,7 +1,61 @@
 <template>
   <h3 class="text text_h3">Мои заказы</h3>
 
+  <div class="order-status">
+
+    <div
+        @click="active = !active"
+        class="row row_al-c row_gap4 pointer"
+    >
+      <div class="text text_normal text_light">Описание статусов</div>
+      <svg
+          :class="{'svg_rotate': active}"
+          class="svg"
+          width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17 14.1973L12 9.19727L7 14.1973" stroke="#89939F" stroke-width="1.5" stroke-linecap="round"
+              stroke-linejoin="round"/>
+      </svg>
+    </div>
+
+    <div
+        :class="{'order-status__list_active' : active}"
+        class="order-status__list"
+    >
+      <div class="column column_gap12">
+        <div class="tip tip_active tip_small">Обработка</div>
+        <div class="text text_normal">Ожидает обработки заказа менеджером</div>
+      </div>
+
+      <div class="column column_gap12">
+        <div class="tip tip_danger tip_small">Отменен</div>
+        <div class="text text_normal">Заказ отменен по каким-либо причинам</div>
+      </div>
+
+      <div class="column column_gap12">
+        <div class="tip tip_active tip_small">Ожидает оплаты</div>
+        <div class="text text_normal">В ожидании поступления средств на расчетный счет организации</div>
+      </div>
+
+      <div class="column column_gap12">
+        <div class="tip tip_dark-fill tip_small">Оплачено</div>
+        <div class="text text_normal">Заказ оплачен, доступ к материалам будет открыт в течении суток</div>
+      </div>
+
+      <div class="column column_gap12">
+        <div class="tip tip_accent tip_small">Проверка документов</div>
+        <div class="text text_normal">Проверяются загруженные докмуенты</div>
+      </div>
+
+      <div class="column column_gap12">
+        <div class="tip tip_dark-fill tip_small">Обучение</div>
+        <div class="text text_normal">Открытый доступ к учебному материалу</div>
+      </div>
+    </div>
+  </div>
+
+
   <div class="order-list">
+
     <div class="order-card">
 
       <div class="row row_jc-sb">
@@ -77,13 +131,12 @@
 
 </template>
 
-<script>
-import ProgramCardTip from "../../ProgramCardTip.vue";
+<script setup>
+import {ref} from "vue";
 
-export default {
-  name: "orders",
-  components: {ProgramCardTip}
-}
+const active = ref(false);
+
+
 </script>
 
 <style lang="less" scoped>
@@ -107,7 +160,7 @@ export default {
 
 .order-list {
   margin-top: 40px;
-  
+
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -121,5 +174,55 @@ export default {
   @media @min580 {
     width: auto;
   }
+}
+
+.order-status {
+  margin-top: 20px;
+}
+
+.order-status__list {
+  max-height: 0;
+  transition: @dur250;
+  overflow: hidden;
+  opacity: 0;
+
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  box-sizing: border-box;
+  padding: 0 20px;
+
+  margin-top: 12px;
+
+  background: @LightGreyColor;
+  border-radius: 8px;
+
+  @media @min760 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 32px 20px;
+  }
+
+}
+
+.order-status__list_active {
+  max-height: 750px;
+  transition: @dur250;
+  padding: 20px;
+  opacity: 1;
+
+  @media @min760 {
+    max-height: 400px;
+  }
+}
+
+.svg {
+  transition: @dur150;
+}
+
+.svg_rotate {
+  transform: rotate(-180deg);
+  transition: @dur150;
 }
 </style>
