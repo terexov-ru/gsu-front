9ae82c1f9db4382cc18c69a96723e152459d4b61
RGABear <russiangamebear@mail.ru>
2024-12-02 23:05:11 +0300

    feat(form): add google form

---
+++ pages/form/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper form-block wrapper_paddings">
+    <section class="form-wrapper">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScfcCo9vp5whLz_WD87C277xifVE6zZ8yth4pz_cvRDrxphCQ/viewform?embedded=true"
+        width="100%"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        scrolling="no"
+        ref="iframe"
+        >Загрузка…</iframe
+      >
+    </section>
+  </div>
+</template>
+
+<script setup>
+const iframe = ref(null);
+
+onMounted(() => {
+  const adjustIframeHeight = () => {
+    console.log("heigt");
+    console.log(iframe.value.contentWindow.document.body.scrollHeight);
+
+    if (iframe.value) {
+      iframe.value.style.height = `${iframe.value.contentWindow.document.body.scrollHeight}px`;
+    }
+  };
+
+  // Подключение к событию загрузки iframe, чтобы установить высоту
+  iframe.value.onload = adjustIframeHeight;
+});
+</script>
+
+<style lang="less" scoped>
+@import "assets/core.less";
+
+.form-block {
+  margin-top: 80px;
+  margin-bottom: 120px;
+}
+
+.form-wrapper {
+  //   margin-top: 32px;
+
+  width: 100%;
+
+  iframe {
+    height: 1700px;
+
+    @media @min580 {
+      height: 1400px;
+    }
+  }
+}
+</style>
