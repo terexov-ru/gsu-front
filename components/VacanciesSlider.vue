1aac8b9d3e0b7f73b9995b7111efe240d967c296
Alexander Maltsev <orangealexmaltsev@gmail.com>
2025-01-29 12:52:32 +0300

    feature: change vacancies text

--- components/VacanciesSlider.vue
+++ components/VacanciesSlider.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="wrapper">
     <div class="slider vacancies__slider">
-
       <h2 class="slider__title">
         <div class="row row_gap10 row_al-c">
           <div>Станьте</div>
           <div class="text-rotate">частью команды</div>
         </div>
-        <div>
-          Университета "Гарант-Сервис"
-        </div>
+        <div>ГСУ</div>
       </h2>
 
-      <div style="flex-basis: 100%"/>
+      <div style="flex-basis: 100%" />
 
       <button
-          @click="this.$emit('clickButton')"
-          class="button slider__button button_dark button_size">
+        @click="this.$emit('clickButton')"
+        class="button slider__button button_dark button_size"
+      >
         Подать заявку
       </button>
 
-
-      <img class="slider__image" src="~/assets/vacancies_sldier.png" alt="banner">
-
+      <img
+        class="slider__image"
+        src="~/assets/vacancies_sldier.png"
+        alt="banner"
+      />
     </div>
   </div>
 </template>
@@ -37,7 +37,12 @@
 @import "assets/core.less";
 
 .vacancies__slider {
-  background: radial-gradient(142.4% 127.35% at 111.75% 91.51%, #FCFF59 0%, rgba(255, 194, 138, 0.10) 100%), linear-gradient(125deg, #007AEA 9.39%, #24FFD8 77.39%);
+  background: radial-gradient(
+      142.4% 127.35% at 111.75% 91.51%,
+      #fcff59 0%,
+      rgba(255, 194, 138, 0.1) 100%
+    ),
+    linear-gradient(125deg, #007aea 9.39%, #24ffd8 77.39%);
 }
 
 .slider__button {
